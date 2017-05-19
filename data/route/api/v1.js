063a4a5f3cef7cd8c7dd4edb792afa2ec9212640
azbn <i@azbn.ru>
2017-05-19 14:35:55 +0300

    201705191435

--- data/route/api/v1.js
+++ data/route/api/v1.js
@@ -6,10 +6,34 @@ function _(app, azbn) {
 	
 	return function(req, res) {
 		
+		var p = req.params;
 		var q = req.query;
 		
 		res.send({
-			test : azbn.now(),
+			meta : {
+				version : 1,
+				version_api : 1,
+				created_at : azbn.now(),
+				created_at_str : '',
+				platform : 'AAE',
+				access : {
+					access_as : null,
+					id : 0,
+				},
+				msg : {
+					type : 'info',
+					text : '',
+				},
+				need : {
+					reload : 0,
+				},
+				notifies : [],
+			},
+			response : {
+				params : req.params,
+				query : req.query,
+				body : req.body,
+			},
 		});
 		
 	};
