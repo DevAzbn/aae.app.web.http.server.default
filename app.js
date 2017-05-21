158954ac27317a97abdc5c414fa619892eff2182
Alexander <alexander.zybin@gmail.com>
2017-05-21 08:27:04 +0300

    201705210827

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-var azbn = new require(__dirname + '/../../../../../system/bootstrap')({
+var azbn = new require(__dirname + '/../../../../../../system/bootstrap')({
 	
 });
 
